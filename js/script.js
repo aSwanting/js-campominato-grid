2dfb7a67f89262ece58fa1adeee7087a4acf7268
aSwanting <gab.damico@gmail.com>
2023-10-13 19:08:31 +0200

    add: grid gen btn, foreach click, color, log i

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
 
+function generateGrid() {
 
-const appMain = document.getElementById("app-main")
+    const gridWrapper = document.getElementById("grid-wrapper")
+    gridWrapper.innerHTML = ""
 
-
-
-
-
-for (i = 0; i < 100; i++) {
-
-    const box = document.createElement("div")
-    appMain.append(box)
-    box.className = "box"
-    box.innerHTML = i + 1
+    for (i = 0; i < 100; i++) {
+        const box = document.createElement("div")
+        gridWrapper.append(box)
+        box.className = "tile"
+        box.innerHTML = i + 1
+    }
 }
+
+function cliccableTiles() {
+
+    const tiles = document.querySelectorAll(".tile")
+
+    tiles.forEach(function (tile, i) {
+        tile.addEventListener("click", function () {
+            tile.style.backgroundColor = "rgba(66, 45, 200, 0.5)"
+            console.log(i + 1)
+        })
+    })
+
+}
+
+document.getElementById("play-button").addEventListener("click", function () {
+
+    generateGrid()
+    cliccableTiles()
+
+})
+
